a441627ed80426163d5f324f9c7d7bb6801d081f
Dave Paiva <mrdavepaiva@gmail.com>
2025-04-03 17:16:39 +0530

    Initial Commit

---
+++ Mapverse/MapverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapverseApp.swift
+//  Mapverse
+//
+//  Created by Dave Paiva on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MapverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
